04f526915c353f4d09258841bd3f87107eb74b44
amirnak <amirnakgamer@gmail.com>
2024-07-08 16:29:49 +0300

    Add: Animation for select pages

--- js/exchange.js
+++ js/exchange.js
@@ -3,3 +3,25 @@ let left_nav = document.getElementById("left-nav")
 left_nav.onclick = function () {
     document.location = "index.html" 
 }
+
+let select_page_first = document.getElementById("first-button")
+let select_page_second = document.getElementById("second-button")
+let select_page_third = document.getElementById("third-button")
+
+select_page_first.onclick = function () {
+    select_page_second.classList.remove("selected")
+    select_page_third.classList.remove("selected")
+    select_page_first.classList.add("selected")
+}
+
+select_page_second.onclick = function () {
+    select_page_first.classList.remove("selected")
+    select_page_third.classList.remove("selected")
+    select_page_second.classList.add("selected")
+}
+
+select_page_third.onclick = function () {
+    select_page_first.classList.remove("selected")
+    select_page_second.classList.remove("selected")
+    select_page_third.classList.add("selected")
+}
